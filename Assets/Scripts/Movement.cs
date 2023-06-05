a303eb793355c9ac385c94ee62635452701f87dd
lexHOME <66259@selwyn.school.nz>
2023-06-06 01:35:46 +1200

    Movement Updates
    -Underwater Bindings Disable
    -Actually good jumping
    -Jumping still needs to be fixed, you can infinite jump

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,42 +8,82 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] 
-    Vector3 movementVector;
-    Vector2 contextMovementVector;
-    Vector3 playerDirection;
-    Rigidbody playerBody;
-    public int walkSpeed = 100;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 playerDirection;
+    [SerializeField] Rigidbody playerBody;
+    [SerializeField] PlayerInput playerInput;
+    [SerializeField] Collider playerCollider;
+    bool isUnderwater;
+    public int walkSpeed;
+    public int jumpForce;
 
     // Start is called before the first frame update
     void Start()
     {
         playerBody = gameObject.GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+        playerInput.actions["Underwater Bindings"].Disable();
+        playerCollider = GetComponent<Collider>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
+        playerBody.AddForce(Physics.gravity,ForceMode.Acceleration);
         MovePlayer();
     }
-
     public void ReadInputValue(InputAction.CallbackContext movementContextInformation)
     {
-        Debug.Log(movementContextInformation);
-        contextMovementVector = movementContextInformation.ReadValue<Vector2>();
-        movementVector = new Vector3(contextMovementVector.x,0,contextMovementVector.y);
-        Debug.Log("Movement vector is: " + movementVector.ToString());
-        
+        string actionTriggeredName = movementContextInformation.action.name;
+        Debug.Log(movementContextInformation.action);
+        switch (actionTriggeredName)
+        {
+            case "Base Movement":
+                Debug.Log("movement");
+                Vector2 contextMovementVector = movementContextInformation.ReadValue<Vector2>();
+                movementVector = new Vector3(contextMovementVector.x,0,contextMovementVector.y);
+
+                //Debug.Log("Movement vector is: " + movementVector.ToString());
+                MovePlayer();
+                break;
+
+            case "Jump":
+                if (movementContextInformation.canceled) return;
+                Debug.Log("jumpy jump");
+                bool groundCheck = Physics.CheckBox(playerCollider.bounds.center, 
+                new Vector3(playerCollider.bounds.center.x, playerCollider.bounds.min.y + -0.1f , playerCollider.bounds.center.z), 
+                quaternion.identity,0);
+
+                Debug.Log(groundCheck);
+                if (!groundCheck) return;
+                playerBody.AddForce(transform.up * jumpForce, ForceMode.VelocityChange);
+                break;
+
+            case "Underwater Bindings": 
+                if (!isUnderwater) return;
+                Debug.Log("underwater");
+                float contextMovementFloat = movementContextInformation.ReadValue<float>();
+                movementVector = new Vector3(0, contextMovementFloat, 0);
+                MovePlayer();
+                break;
+
+            default:
+                Debug.Log("uh oh god i dont know its none of the action names");
+                break;
+        }
     }
    
     void MovePlayer()
     {
-        if (movementVector == Vector3.zero) { return; }
+        if (movementVector == Vector3.zero) return; 
 
         playerDirection = movementVector * walkSpeed * Time.fixedDeltaTime;
-        Debug.Log("Player Direction is: " + playerDirection.ToString());
-        Debug.Log("=================");
+
         playerBody.AddForce(playerDirection, ForceMode.VelocityChange);
     }
    
+   private void OnDrawGizmos()
+   {
+    Gizmos.DrawCube(playerCollider.bounds.center,
+    new Vector3(playerCollider.bounds.center.x, playerCollider.bounds.min.y + -0.1f , playerCollider.bounds.center.z));
+   }
 }
