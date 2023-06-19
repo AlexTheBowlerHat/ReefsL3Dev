77c92f191ed3fd741410724180e4380ddc5c357f
AlexTheBowlerHat <66259@selwyn.school.nz>
2023-06-20 11:39:07 +1200

    Attempt at underwater detection

--- Assets/Scripts/PlayerDetection.cs
+++ Assets/Scripts/PlayerDetection.cs
@@ -1,12 +1,29 @@
+using Mono.Cecil;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerDetection : MonoBehaviour
 {
+    Camera sceneCamera;
+    public GameObject player;
+    Transform playerTransform;
+    Collider playerCollider;
+    
+
+    public Transform oceanPlaneTransform;
+    public GameObject globalVolume;
+
+    public Component[] underwaterFilters;
+
     // Start is called before the first frame update
     void Start()
     {
+        sceneCamera = Camera.main;
+        playerCollider = player.GetComponent<Collider>();
+        playerTransform = player.transform;
         
     }
 
@@ -16,8 +33,12 @@ public class PlayerDetection : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter (Collider otherCollider)
     {
-        
+        if (!otherCollider == playerCollider ) return;
+        Vector3 cameraWorldSpacePosition = sceneCamera.ScreenToWorldPoint(new Vector3(Screen.width/2, Screen.height/2, sceneCamera.transform.position.z));
+
+
+        Debug.Log("oog");
     }
 }
