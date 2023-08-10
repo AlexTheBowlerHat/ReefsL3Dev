85fd3faf89a5b53430f4a98d42c66f798d9c1c99
AlexTheBowlerHat <66259@selwyn.school.nz>
2023-08-11 09:35:26 +1200

    This dialogue system is a failure

--- Assets/Scripts/DialogueHandler.cs
+++ Assets/Scripts/DialogueHandler.cs
@@ -10,18 +10,29 @@ public class DialogueHandler : MonoBehaviour
     [SerializeField]
     private UIDocument DialogueUI;
     UnityEngine.UIElements.Label dialogueLabel;
+    Button dialogueButton;
     [SerializeField] 
     List<string> splitDialogue;
     bool addingText = false;
+
+    bool continueTextAlong = false;
     // Start is called before the first frame update
     void Start()
     {
         var rootUIElement = DialogueUI.rootVisualElement;
         dialogueLabel = rootUIElement.Q<UnityEngine.UIElements.Label>("Dialogue");
+        dialogueButton = rootUIElement.Q<UnityEngine.UIElements.Button>("ClickToContinue");
+
+        dialogueButton.SetEnabled(false);
+        dialogueButton.RegisterCallback<ClickEvent>(ev => ContinueText());
         dialogueLabel.visible = false;
         dialogueLabel.text = "";
     }
-
+    void ContinueText()
+    {
+        continueTextAlong = true;
+        dialogueButton.SetEnabled(false);
+    }
     IEnumerator TextTypingEffect(string dialogue)
     {
         while (addingText)
@@ -34,9 +45,16 @@ public class DialogueHandler : MonoBehaviour
         {
             dialogueLabel.text += character;
             //Debug.Log("typing effect with char " + character);
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(0.05f);
         }
-        //TODO ADD PAUSE HERE WHERE YOU CLICK TO CONTINUE
+
+        //Pause Effect
+        dialogueButton.SetEnabled(true);
+        while (!continueTextAlong)
+        {
+            yield return new WaitForSeconds(0.01f);
+        }
+        continueTextAlong = false;
         dialogueLabel.text = "";
         addingText = false;
         yield break;
@@ -48,7 +66,6 @@ public class DialogueHandler : MonoBehaviour
 
         foreach (var line in splitDialogue) 
         {
-            Debug.Log("change dialogue foreach loop");
             StartCoroutine(TextTypingEffect(line));
             yield return new WaitForSeconds(0.2f);
         }
@@ -61,19 +78,22 @@ public class DialogueHandler : MonoBehaviour
 
         foreach (string sentence in sentenceSplitDialogue)
         {
+            Debug.Log("Sentence in splitter is: " + sentence);
             splitDialogue.Add(sentence);
-            LongStringSplitter(sentence);
         }
 
     }
+    /*
     //I counted, the rough length of a line in the text box is 135
     void LongStringSplitter(string sentence)
     {
-        if (sentence.Length <= 135) return;
-        string[] splitLongSentenceArray = sentence.Split(sentence[134]);
+       
+
         foreach (string stringsplitLongSentence in splitLongSentenceArray)
         {
+            Debug.Log("Long string bit split is: " + stringsplitLongSentence);
             splitDialogue.Add(stringsplitLongSentence);
         }
     }
+    */
 }
