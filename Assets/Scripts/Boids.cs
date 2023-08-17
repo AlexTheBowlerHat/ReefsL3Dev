2337b30abef2ab1c9875fb8d6897e280e4e5d5d9
AlexTheBowlerHat <66259@selwyn.school.nz>
2023-08-18 09:39:38 +1200

    Boid + humanoid start

--- Assets/Scripts/Boids.cs
+++ Assets/Scripts/Boids.cs
@@ -6,35 +6,84 @@ using UnityEngine.UIElements;
 
 public class Boids : MonoBehaviour
 {
-    float boidMass = 0f;
-    //Vector 3 boidPosition gameObject.transform.position
-    Vector3 boidVelocity = Vector3.zero; 
-    float boidMaxForce = 0f;
-    float boidMaxSpeed = 0f;
-    //Quaternion Orientation quaternion.identity;
+    Vector3 boidVelocity = Vector3.zero;
+    public float boidSpeed = 5f;
+
+    Quaternion boidOrientation;
     //===============
+    float boidDectionRadius = 5f;
 
     Vector3 steeringDirection = Vector3.zero;
-    Vector3 steeringForce = Vector3.zero; 
+    Vector3 steeringForce = Vector3.zero;
+    Rigidbody boidBody;
+    SphereCollider boidNeighbourCollider;
+    List<Vector3> nearBoidPositions;
+
+    //steeringforce = truncate (steering_direction, max_force)
+    //acceleration = steering_force / mass
+    //float boidMaxForce = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        boidMass = gameObject.GetComponent<Rigidbody>().mass;
+        boidBody = gameObject.GetComponent<Rigidbody>();
+        //boidMass = boidBody.mass;
+        boidOrientation = quaternion.identity;
+
+        boidNeighbourCollider = gameObject.GetComponent<SphereCollider>();
+        boidNeighbourCollider.radius = boidDectionRadius;
     }
 
     // Update is called once per frame
     void Update()
     {
-        steeringForce = Vector3.ClampMagnitude(steeringForce, boidMaxForce);
         UpdateMovement();
+
     }
 
     void UpdateMovement()
     {
-        //steeringforce = truncate (steering_direction, max_force)
-        //acceleration = steering_force / mass
+        Vector3 seperateVector = Seperate();
+        boidVelocity = (transform.forward.normalized + seperateVector.normalized) * boidSpeed;
+        gameObject.transform.position += (boidVelocity * Time.deltaTime);
+
+
         //boidVelocity = truncate (velocity + acceleration, max_speed)
         //boidPosition = position + velocity
+        //steeringForce = Vector3.ClampMagnitude(steeringForce, boidMaxForce);
     }
+    Vector3 Seperate()
+    {
+        Vector3 seperateVector = Vector3.zero;
+        foreach (Vector3 neighbourBoidPosition in nearBoidPositions) 
+        {
+            Vector3 toNeibourBoidVector = neighbourBoidPosition - gameObject.transform.position;
+            //something about the magnitude (shorter is better priority)
+            //maybe like find the smallest and largest vectors, then the magnitude difference is what its compared to
+            //and adding them together before inversing the vector
+        }
+
+        return seperateVector;
+    }
+    void Align()
+    {
+
+    }
+    void StickTogether()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Boid") { return; }
+        nearBoidPositions.Add(other.transform.position);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag != "Boid") { return; }
+        nearBoidPositions.Remove(other.transform.position);
+    }
+
 }
