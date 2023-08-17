cdd062c6a4fed3e05bbd6373833c85bb7e114785
AlexTheBowlerHat <66259@selwyn.school.nz>
2023-08-17 13:35:58 +1200

    Boid plus coral

--- Assets/Scripts/Boids.cs
+++ Assets/Scripts/Boids.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Boids : MonoBehaviour
 {
     float boidMass = 0f;
-    //Vector 3 Position gameObject.transform.position
+    //Vector 3 boidPosition gameObject.transform.position
+    Vector3 boidVelocity = Vector3.zero; 
     float boidMaxForce = 0f;
     float boidMaxSpeed = 0f;
     //Quaternion Orientation quaternion.identity;
     //===============
 
     Vector3 steeringDirection = Vector3.zero;
-    Vector3 steeringForce = Vector3.zero;
+    Vector3 steeringForce = Vector3.zero; 
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +32,9 @@ public class Boids : MonoBehaviour
 
     void UpdateMovement()
     {
-
+        //steeringforce = truncate (steering_direction, max_force)
+        //acceleration = steering_force / mass
+        //boidVelocity = truncate (velocity + acceleration, max_speed)
+        //boidPosition = position + velocity
     }
 }
