fefb3314199ee2de19f19109da8a2a8d3a117cf2
ABowlerHat <66259@selwyn.school.nz>
2023-10-12 13:36:19 +1300

    Further Analysis script dev

--- Assets/Scripts/AnalyseFish.cs
+++ Assets/Scripts/AnalyseFish.cs
@@ -1,15 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.InputSystem;
 
 public class AnalyseFish : MonoBehaviour
 {
     [SerializeField] float raycastDistance = 0f;
     [SerializeField] float keyPresslength = 0f;
+    [SerializeField] int layerMaskLayer = 0;
+    GameObject playerGameObject;
+    PlayerInput playerInput;
 
+    public UIDocument defaultGUIDocument;
+    VisualElement rootUIElement;
+
+    void DisableAnalysis()
+    {
+        playerInput.actions.FindAction("Analyse").Disable();
+        //UI transparency change here
+    }
     void RayHitFishCheck()
     {
-        Vector3 distanceToFish = Vector3.zero;
+        RaycastHit analysisInfo;
+
+       //Guard clause ends method early if nothing hit with the ray
+       if (!Physics.Raycast(origin: gameObject.transform.position,
+            direction: Camera.main.transform.forward,
+            hitInfo: out analysisInfo,
+            maxDistance: raycastDistance,
+            layerMask: layerMaskLayer))
+        {
+            DisableAnalysis();
+            return;
+        }
+
+        //Pulls up key indication UI + enable key
+        playerInput.actions.FindAction("Analyse").Enable();
+        //UI transparency change here
+
+    }
+
+    void Start()
+    {
+        playerGameObject = GameObject.FindGameObjectWithTag("Player");
+        playerInput = playerGameObject.GetComponent<PlayerInput>();
+        rootUIElement = defaultGUIDocument.rootVisualElement;
     }
 
     // Update is called once per frame
